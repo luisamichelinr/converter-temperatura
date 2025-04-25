06394bed2599d4582e45a91a9aef2f3a7e517157
luisamichelinr <luisa.michelinr@gmail.com>
2025-04-25 14:08:55 -0300

    Tratamento de erro
    Foi incorporado o tratamento de erro

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def index():
 
 @app.route('/conversao_temperatura', methods=['POST'])
 def conversao_temperatura():
-    celsius = int(request.form['celsius'])
-
-    fahrenheit = round((celsius * 9/5) + 32, 2)
-    kelvin = round(celsius + 273.15, 2)
-
-    return render_template('index.html', fahrenheit=fahrenheit, kelvin=kelvin)
+    try:
+        celsius = int(request.form['celsius'])
+        fahrenheit = round((celsius * 9/5) + 32, 2)
+        kelvin = round(celsius + 273.15, 2)
+        return render_template('index.html', fahrenheit=fahrenheit, kelvin=kelvin)
+    except Exception as e:
+        fahrenheit = f'Ocorreu um erro inesperado {e}'
+        kelvin = f'Ocorreu um erro inesperado {e}'
+        return render_template('index.html', fahrenheit=fahrenheit, kelvin=kelvin)
 
 if __name__ == '__main__':
     app.run(debug=True)
